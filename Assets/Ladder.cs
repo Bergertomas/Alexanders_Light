efb59e22e47bb7c4fb0bf6878b6dc9382d319114
Tomer Ram <ramtomer0@gmail.com>
2019-10-30 20:18:09 +0200

    Added basic and non-working ladder script. we should move it to PlayerController to allow more control on gravity during climb. Found critical bug with triggers and light detection

---
+++ Assets/Ladder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    bool canClimb = false;
+    float speed = 20.0f;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canClimb = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canClimb = false;
+        }
+    }
+    void Update()
+    {
+        if (canClimb)
+        {
+            var moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            var moveClimb = new Vector2(0, moveY);
+            player.transform.Translate(moveClimb * Time.deltaTime * speed);
+        }
+    }
+
+}
