840e963686db3dcec7f523994ea63717c3b11c2f
Semimartian <semimartian@hotmail.com>
2019-11-07 15:40:18 +0200

    Introduced camera rapists

---
+++ Assets/CameraRapist.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRapist : MonoBehaviour
+{
+    public Transform Centre;
+    public int RapistStrength = 1;
+    public int PlayerStrength = 1;
+    void Start()
+    {
+        MeshRenderer[] graphics= GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < graphics.Length; i++)
+        {
+            graphics[i].enabled = false;  
+        }
+    }
+}
---
+++ Assets/CameraRapistCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRapistCollider : MonoBehaviour
+{
+    private CameraRapist owner;
+    void Start()
+    {
+        this.owner = GetComponentInParent<CameraRapist>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Rapist penetrated!");
+        if (other.gameObject.tag == "Player")
+        {
+            CameraController.Instance.AddRapist(owner);      
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Rapist receded!");
+        if (other.gameObject.tag == "Player")
+        {
+            CameraController.Instance.RemoveRapist(owner);
+            //CameraController.Instance.LowerSpeed();
+        }
+    }
+}
