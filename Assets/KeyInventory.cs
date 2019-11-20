4d6cec11d96b3f406eac255a3c83baedd8f9caa6
Tomer Ram <ramtomer0@gmail.com>
2019-11-20 18:39:01 +0200

    Added simple pickup mechanic for key items, and adding them to key inventory (put KeyInventory script on player, put KeyItem script on the key object

---
+++ Assets/KeyInventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyInventory : MonoBehaviour
+{
+    KeyItem collectedKey;
+    [SerializeField]
+    Dictionary<string, bool> keyBag = new Dictionary<string, bool>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Key")
+        {
+            collectedKey = other.gameObject.GetComponent<KeyItem>();
+            Destroy(other.gameObject);
+            keyBag.Add(collectedKey.keyname, collectedKey.keyActive);
+            collectedKey = null;
+        }
+    }
+}
---
+++ Assets/KeyItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    public string keyname;
+    public bool keyActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "Key";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
