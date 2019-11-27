5b46e5c2d7543f9663fc2941ff30a5893a406da6
Tomer Ram <ramtomer0@gmail.com>
2019-11-27 16:06:19 +0200

    Added key-lock mechanic

--- Assets/KeyInventory.cs
+++ Assets/KeyInventory.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class KeyInventory : MonoBehaviour
 {
-    KeyCharger targetCharger;
     KeyItem someKey = null;
+    KeyCharger targetCharger;
+    KeyLock targetLock;
     [SerializeField]
     Dictionary<string, KeyItem> keyBag = new Dictionary<string, KeyItem>();
 
@@ -26,20 +27,49 @@ public class KeyInventory : MonoBehaviour
     {
         if (other.gameObject.tag == "Key")
         {
-            Debug.Log("found key");
             someKey = other.gameObject.GetComponent<KeyItem>();
+            Debug.Log("found keyname " + someKey.keyname);
             Destroy(other.gameObject);
             keyBag.Add(someKey.keyname, someKey);
-            Debug.Log(keyBag[someKey.keyname].keyActive);
             someKey = null;
         }
 
         if (other.gameObject.tag == "KeyCharger")
         {
             targetCharger = other.gameObject.GetComponent<KeyCharger>();
-            keyBag[targetCharger.chargeableKey].keyActive = true;
-            Debug.Log(keyBag[targetCharger.chargeableKey].keyActive);
+            if (keyBag.ContainsKey(targetCharger.chargeableKey))
+            {
+                keyBag[targetCharger.chargeableKey].keyActive = true;
+                Debug.Log(targetCharger.chargeableKey + " is charged");
+            }
+            else
+            {
+                Debug.Log("No key to charge");
+            }
             targetCharger = null;
+        }
+
+        if (other.gameObject.tag == "KeyLock")
+        {
+            targetLock = other.gameObject.GetComponent<KeyLock>();
+            if (keyBag.ContainsKey(targetLock.rightKey))
+            {
+                if (keyBag[targetLock.rightKey].keyActive == true)
+                {
+                    targetLock.unlocked = true;
+                    keyBag.Remove(targetLock.rightKey);
+                    Debug.Log("unlocked with keyname " + targetLock.rightKey);
+                }
+                else
+                {
+                    Debug.Log("key is not charged");
+                }
+            }
+            else
+            {
+                Debug.Log("No key");
+            }
+            targetLock = null;
         }
     }
 }
---
+++ Assets/KeyLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLock : MonoBehaviour
+{
+    public string rightKey;
+    public bool unlocked = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "KeyLock";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
